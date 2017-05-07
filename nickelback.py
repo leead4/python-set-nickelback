f18327d85e7177e6d962e0b9a763b7d94b5d3693
Angela Lee <leead4@gmail.com>
2017-05-07 15:02:07 -0500

    first commit

---
+++ nickelback.py
@@ -0,0 +1,47 @@
+# -------------------------------------------------
+# n this exercises, you're going to use a conditional statement 
+# inside a comprehension. Let's look at a basic example.
+
+# conditional example-------------------
+
+
+nums = range(10)
+
+small_numbers = [num for num in nums if num < 6]
+print(small_numbers)
+
+# Only add numbers to the new list if the value is less than 6
+
+# words = ['big', 'red', 'dog', 'ate', 'his', 'food']
+# three_letters_words = [ word.title() for word in words if len(word) == 3 ]
+# # len(stringVariable) is equivalent to stringVariable.length in JavaScript
+
+
+# --------------------------------------------------
+# Define a set that contains tuples. Each tuple should contain two strings:
+
+# The name of an artist
+# A song by that artist
+# Make sure that some of the songs are from the band Nickelback. 
+# You can see a list of their greatest hits on Amazon.
+
+# # Example set
+# songs = {
+#     ('Nickelback', 'How You Remind Me'), 
+#     ('Will.i.am', 'That Power'),
+#     ('Miles Davis', 'Stella by Starlight'),
+#     ('Nickelback', 'Animals')
+# }
+# Using a set comprehension, create a new set that contains all songs that were not performed by Nickelback.
+
+# list = [] has order 
+# set = {} isn't ordered with unqiue values
+
+songs = {('Nickelback', 'How You Remind Me'), ('Will.i.am', 'That Power'), ('Miles Davis', 'Stella by Starlight'), ('Nickelback', 'Animals')}
+
+not_nickelback = {song for song in songs if song[0] != 'Nickelback'}
+print(not_nickelback)
+
+ 
+# better_songs = [song for song in songs if len(song) == 10]
+# print(better_songs)
